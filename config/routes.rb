927dc3601f869ea95ba46fa25ac6eaec4c39980c
German Escobar <german.escobarc@gmail.com>
2015-08-31 09:10:05 -0500

    Implementa la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-
-  resources :products
-
   root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
+  resources :products
 end
